1a3ec42edff4c2bee9a1076e3fcf5f1ddb7582d1
Masaya. K <ac2393921@gmail.com>
2020-09-24 21:08:57 +0900

    ref: 各種リファクタリング

---
+++ resources/js/components/Todo/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+function Todo() {
+  return (
+    <div className="todo">
+      <Header />
+
+      <Main />
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Todo
