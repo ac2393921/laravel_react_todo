341709e73f3bd03017da70065fb2b8b2b8ca0620
Masaya. K <ac2393921@gmail.com>
2020-09-27 17:12:36 +0900

    fix: userモデルの参照先を変更

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
